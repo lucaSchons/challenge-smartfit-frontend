a63d98ea22b29fe7401ace69be73cbc0c000ada4
lucaSchons <lucasgschons@gmail.com>
2023-10-24 23:56:34 -0300

    configuração

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { formComponent } from './form-component/form.component';
+import { listcardComponent } from './list-card/list-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    formComponent,
+    listcardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
